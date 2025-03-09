82f676a4e9ec873626fe5fbd0ad25de90a6f4326
yoann256 <>
2025-03-09 16:23:46 +0100

    Last Sync: 2025-03-09 15:23:46 (Mobile)

---
+++ html/js/main.js
@@ -0,0 +1,11 @@
+const githubMain = document.querySelector(".github-main");
+const githubOrg = document.querySelector(".github-org");
+
+githubMain.addEventListener("click", function () {
+    window.location.href = "https://github.com/yoann256/scratchapi-experiments";
+});
+
+githubOrg.addEventListener("click", function () {
+    window.location.href = "https://github.com/Scratch-Experiments-Group";
+});
+
