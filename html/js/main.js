b354229b3139e003326d6ab89b4478c4305d0838
yoann256 <>
2025-03-11 20:48:22 +0100

    Last Sync: 2025-03-11 19:48:22 (Mobile)

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,40 @@
+// Declare some HTML elements
+const pageBody = document.querySelector("body");
+const pageHead = document.querySelector("head");
+
+console.log("Main HTML elements successfully declared!");
+
+// Declare other elements
 const githubMain = document.querySelector(".github-main");
 const githubOrg = document.querySelector(".github-org");
+const reloadButton = document.querySelector(".reload-button");
 
-githubMain.addEventListener("click", function () {
-    window.location.href = "https://github.com/yoann256/scratchapi-experiments";
-});
+console.log("Other HTML elements successfully declared!");
 
-githubOrg.addEventListener("click", function () {
-    window.location.href = "https://github.com/Scratch-Experiments-Group";
-});
+try {
+    if (githubMain) {
+        githubMain.addEventListener("click", function () {
+            window.location.href = "https://github.com/yoann256/scratchapi-experiments";
+        });
+    } else {
+        console.error("githubMain was not found or declared as null!");
+    }
 
+    if (githubOrg) {
+        githubOrg.addEventListener("click", function () {
+            window.location.href = "https://github.com/Scratch-Experiments-Group";
+        });
+    } else {
+        console.error("githubOrg was not found or declared as null!");
+    }
+    
+      if (reloadButton) {
+        reloadButton.addEventListener("click", function () {
+            window.location.reload();
+        });
+    } else {
+        console.error("reloadButton was not found or declared as null!");
+    }
+} catch (error) { // FIX: Added 'error' parameter
+    console.warn("Please run this script in a web browser environment! HTML/CSS/JS");
+}
